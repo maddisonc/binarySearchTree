6d3a9ef440379215a6b1d67166518558aa8c208b
Maddison <mtranchau1@gmail.com>
2023-02-15 18:51:30 -0800

    2/15/23: 6:50pm: Changed driver class to be in binarySearchTree.java for convenience, created nonfunctional nodeSorting class template that will act as the method for sorting values (placing new nodes to the left or right under the parent.)

--- src/binarySearchTree.java
+++ src/binarySearchTree.java
@@ -1,9 +1,35 @@
+// tree is made of nodes, left and right nodes connect to original one
+// value of left node is less than the parent, right value is greater than the parent
+class Node
+{
+    int ogValue;
+    Node left;
+    Node right;
+} // end node class
+
+// method that will sort the new nodes based on value (place left or right below parent)
+class nodeSorting
+{
+    public Node insertVals (Node node, int newVal)
+    {
+        if (node == null)
+        {
+            // write method that creates a new node with value passed in
+            return createNewNode(newVal);
+        }
+    } // end insertion method
+} // end sorting nodes method
+
+
+// acts as driver class
 public class binarySearchTree
 {
-    class Node
+    public static void main(String[] args)
     {
-        int ogValue;
-        Node left;
-        Node right;
-    }
-}
+        nodeSorting tree1 = new nodeSorting();
+        Node root = null;
+
+        // creates new root with value of 10
+        tree1.insertVals(root, 10);
+    } // end main
+} // end driver method
