ca195f455c9d9c7183bbb222bd6e8083af0937f3
Maddison <mtranchau1@gmail.com>
2023-02-15 20:09:02 -0800

    2/15/23: 8:08pm: Deleted driver class (previously used for testing) in favor of using junit tests - reworked printTree method to return a string of the entire tree rather than printing to the console (for junit testing).  Wrote three tests to ensure functioning insertion method, one test for the tree organization.

--- Tests/nodeSortingTest.java
+++ Tests/nodeSortingTest.java
@@ -3,19 +3,52 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class nodeSortingTest {
+    nodeSorting tree1 = new nodeSorting();
+    Node root = null;
 
+    // test inserting value method (when creating a new node, will the correct value be returned?)
     @Test
-    void insertVals()
+    void insertVal10()
     {
+        int expected = 10;
+        int actual = tree1.insertVals(root, 10).ogValue;
+        assertEquals(expected, actual);
     }
 
     @Test
-    void createNewNode()
+    void insertVal8()
     {
+        int expected = 8;
+        int actual = tree1.insertVals(root, 8).ogValue;
+        assertEquals(expected, actual);
     }
 
     @Test
-    void printTree()
+    void insertVal4()
     {
+        int expected = 4;
+        int actual = tree1.insertVals(root, 4).ogValue;
+        assertEquals(expected, actual);
+    }
+
+    // tests accuracy of tree node placement
+    @Test
+    void printTreeRoot10 ()
+    {
+        // starting value: 10
+        root = tree1.insertVals(root, 10);
+
+        root = tree1.insertVals(root, 8);
+        root = tree1.insertVals(root, 4);
+        root = tree1.insertVals(root, 2);
+        root = tree1.insertVals(root, 12);
+        root = tree1.insertVals(root, 24);
+        root = tree1.insertVals(root, 6);
+        root = tree1.insertVals(root, 0);
+
+        // expected tree beginning with 10
+        String expected = ": 10 | left of previous: 8 | left of previous: 4 | left of previous: 2 | left of previous: 0 | right of previous: 6 | right of previous: 12 | right of previous: 24 | ";
+        String actual = tree1.printTree(root, "");
+        assertEquals(expected, actual);
     }
 }
--- src/binarySearchTree.java
+++ src/binarySearchTree.java
@@ -44,65 +44,22 @@ class nodeSorting
     }
 
     // printing method
-    public void printTree(Node node, String leftOrRight)
+    public String printTree(Node node, String leftOrRight)
     {
+        String result = "";
         if (node == null)
         {
-            return;
+            return "";
         }
 
         // prints value with placement (left or right of previous)
-        System.out.println(leftOrRight + ": " + node.ogValue);
+        result += (leftOrRight + ": " + node.ogValue + " | ");
 
         // calls self, if left states this
-        printTree(node.left, "left of previous");
+        result += printTree(node.left, "left of previous");
         // calls self, if right states this
-        printTree(node.right, "right of previous");
+        result += printTree(node.right, "right of previous");
+        return result;
     }
 
 } // end sorting nodes method
-
-
-// acts as driver class
-public class binarySearchTree
-{
-    public static void main(String[] args)
-    {
-        nodeSorting tree1 = new nodeSorting();
-        Node root = null;
-
-        // creates new root with value of 10
-        root = tree1.insertVals(root, 10);
-        System.out.println("Root value: " + 10);
-
-        // left of 10
-        root = tree1.insertVals(root, 8);
-        System.out.println("Added: " + 8);
-        // left of 8
-        root = tree1.insertVals(root, 4);
-        System.out.println("Added: " + 4);
-        // left of 4
-        root = tree1.insertVals(root, 2);
-        System.out.println("Added: " + 2);
-        // right of 6
-        root = tree1.insertVals(root, 12);
-        System.out.println("Added: " + 12);
-        // right of 12
-        root = tree1.insertVals(root, 24);
-        System.out.println("Added: " + 24);
-        // right of 0
-        root = tree1.insertVals(root, 6);
-        System.out.println("Added: " + 6);
-        // left of 2
-        root = tree1.insertVals(root, 0);
-        System.out.println("Added: " + 0);
-
-        System.out.println("\nBinary Search Tree:\n");
-
-        // calls method to print tree
-        System.out.print("Beginning value (root)");
-        tree1.printTree(root, "");
-
-        // Note: order of adding values does not matter, the  smallest value will go to the second least node and position itself to the left
-    } // end main
-} // end driver method
