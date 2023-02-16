45d02accc310962ba434a07d3603e3ab0bdc0570
Maddison <mtranchau1@gmail.com>
2023-02-15 19:40:06 -0800

    2/15/23: 7:39pm: Created test directory with empty tests.

---
+++ Tests/nodeSortingTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class nodeSortingTest {
+
+    @Test
+    void insertVals()
+    {
+    }
+
+    @Test
+    void createNewNode()
+    {
+    }
+
+    @Test
+    void printTree()
+    {
+    }
+}
